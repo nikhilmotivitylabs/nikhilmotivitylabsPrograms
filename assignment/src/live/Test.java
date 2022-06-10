91a95dc83f550a7d1fecfba673e5b09e3d1da20e
nikhilmotivitylabs <106816016+nikhilmotivitylabs@users.noreply.github.com>
2022-06-10 11:38:46 +0530

    commit

---
+++ assignment/src/live/Test.java
@@ -0,0 +1,21 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Playable a = new Veena();
+		a.play();
+		
+		Playable b=new Saxophone();
+		b.play();
+		
+		
+	}
+
+}
---
+++ assignment/src/music/string/Veena.java
@@ -0,0 +1,10 @@
+package music.string;
+
+import music.Playable;
+
+public class Veena implements Playable {
+
+	public void play() {  //we should given pulbic for interface method
+		System.out.println("veena music");
+	}
+}
